02a2a71aa1c5f7fde5c51a5bdb4fe6400559c467
Thomas Huber <huber.thomas@outlook.com>
2019-07-16 20:11:34 +0200

    code visibility

--- train.py
+++ train.py
@@ -169,72 +169,55 @@ def main():
     logger.info('*'*80)
     logger.info('the args are the below')
     logger.info('*'*80)
+    
     for x in args.__dict__:
         logger.info(x+','+str(args.__dict__[x]))
+    
     logger.info(cfg.config[args.dataset])
     logger.info('*'*80)
     os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    
     if not os.path.exists(args.param_dir):
         os.mkdir(args.param_dir)
+    
     torch.manual_seed(long(time.time()))
+
     model = bdcn.BDCN(pretrain=args.pretrain, logger=logger)
+    
     if args.complete_pretrain:
         model.load_state_dict(torch.load(args.complete_pretrain))
+
     logger.info(model)
     train(model, args)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train BDCN for different args')
-    parser.add_argument('-d', '--dataset', type=str, choices=cfg.config.keys(),
-        default='bsds500', help='The dataset to train')
-    parser.add_argument('--param-dir', type=str, default='params',
-        help='the directory to store the params')
-    parser.add_argument('--lr', dest='base_lr', type=float, default=1e-6,
-        help='the base learning rate of model')
-    parser.add_argument('-m', '--momentum', type=float, default=0.9,
-        help='the momentum')
-    parser.add_argument('-c', '--cuda', action='store_true',
-        help='whether use gpu to train network')
-    parser.add_argument('-g', '--gpu', type=str, default='0',
-        help='the gpu id to train net')
-    parser.add_argument('--weight-decay', type=float, default=0.0002,
-        help='the weight_decay of net')
-    parser.add_argument('-r', '--resume', type=str, default=None,
-        help='whether resume from some, default is None')
-    parser.add_argument('-p', '--pretrain', type=str, default=None,
-        help='init net from pretrained model default is None')
-    parser.add_argument('--max-iter', type=int, default=40000,
-        help='max iters to train network, default is 40000')
-    parser.add_argument('--iter-size', type=int, default=10,
-        help='iter size equal to the batch size, default 10')
-    parser.add_argument('--average-loss', type=int, default=50,
-        help='smoothed loss, default is 50')
-    parser.add_argument('-s', '--snapshots', type=int, default=1000,
-        help='how many iters to store the params, default is 1000')
-    parser.add_argument('--step-size', type=int, default=10000,
-        help='the number of iters to decrease the learning rate, default is 10000')
-    parser.add_argument('--display', type=int, default=20,
-        help='how many iters display one time, default is 20')
-    parser.add_argument('-b', '--balance', type=float, default=1.1,
-        help='the parameter to balance the neg and pos, default is 1.1')
-    parser.add_argument('-l', '--log', type=str, default='log.txt',
-        help='the file to store log, default is log.txt')
-    parser.add_argument('-k', type=int, default=1,
-        help='the k-th split set of multicue')
-    parser.add_argument('--batch-size', type=int, default=1,
-        help='batch size of one iteration, default 1')
-    parser.add_argument('--crop-size', type=int, default=None,
-        help='the size of image to crop, default not crop')
-    parser.add_argument('--yita', type=float, default=None,
-        help='the param to operate gt, default is data in the config file')
-    parser.add_argument('--complete-pretrain', type=str, default=None,
-        help='finetune on the complete_pretrain, default None')
-    parser.add_argument('--side-weight', type=float, default=0.5,
-        help='the loss weight of sideout, default 0.5')
-    parser.add_argument('--fuse-weight', type=float, default=1.1,
-        help='the loss weight of fuse, default 1.1')
-    parser.add_argument('--gamma', type=float, default=0.1,
-        help='the decay of learning rate, default 0.1')
+    parser.add_argument('-d', '--dataset', type=str, choices=cfg.config.keys(), default='bsds500', help='The dataset to train')
+    #parser.add_argument('-i', '--inputDir', type=str, default=None, 'Directory of the dataset to train.')
+    parser.add_argument('--param_dir', type=str, default='params', help='the directory to store the params')
+    parser.add_argument('--lr', dest='base_lr', type=float, default=1e-6, help='the base learning rate of model')
+    parser.add_argument('-m', '--momentum', type=float, default=0.9, help='the momentum')
+    parser.add_argument('-c', '--cuda', action='store_true', help='whether use gpu to train network')
+    parser.add_argument('-g', '--gpu', type=str, default='0',  help='the gpu id to train net')
+    parser.add_argument('--weight_decay', type=float, default=0.0002, help='the weight_decay of net')
+    parser.add_argument('-r', '--resume', type=str, default=None, help='whether resume from some, default is None')
+    parser.add_argument('-p', '--pretrain', type=str, default=None, help='init net from pretrained model default is None')
+    parser.add_argument('--max_iter', type=int, default=40000, help='max iters to train network, default is 40000')
+    parser.add_argument('--iter_size', type=int, default=10, help='iter size equal to the batch size, default 10')
+    parser.add_argument('--average_loss', type=int, default=50, help='smoothed loss, default is 50')
+    parser.add_argument('-s', '--snapshots', type=int, default=1000, help='how many iters to store the params, default is 1000')
+    parser.add_argument('--step_size', type=int, default=10000, help='the number of iters to decrease the learning rate, default is 10000')
+    parser.add_argument('--display', type=int, default=20, help='how many iters display one time, default is 20')
+    parser.add_argument('-b', '--balance', type=float, default=1.1, help='the parameter to balance the neg and pos, default is 1.1')
+    parser.add_argument('-l', '--log', type=str, default='log.txt', help='the file to store log, default is log.txt')
+    parser.add_argument('-k', type=int, default=1, help='the k-th split set of multicue')
+    parser.add_argument('--batch_size', type=int, default=1, help='batch size of one iteration, default 1')
+    parser.add_argument('--crop_size', type=int, default=None, help='the size of image to crop, default not crop')
+    parser.add_argument('--yita', type=float, default=None, help='the param to operate gt, default is data in the config file')
+    parser.add_argument('--complete_pretrain', type=str, default=None, help='finetune on the complete_pretrain, default None')
+    parser.add_argument('--side_weight', type=float, default=0.5, help='the loss weight of sideout, default 0.5')
+    parser.add_argument('--fuse_weight', type=float, default=1.1, help='the loss weight of fuse, default 1.1')
+    parser.add_argument('--gamma', type=float, default=0.1, help='the decay of learning rate, default 0.1')
     return parser.parse_args()
 
 if __name__ == '__main__':
